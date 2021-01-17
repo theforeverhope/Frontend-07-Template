70beb4a2befdd0287bbe4309a4ecebc49ef3fd89
theforeverhope <theforeverhope@163.com>
2021-01-17 23:16:26 +0800

    feat: debugging

--- Week08/toy-browser/client.js
+++ Week08/toy-browser/client.js
@@ -1,57 +1,3 @@
-学习笔记
-
-## Browser
-
-URL -HTTP-> HTML -parse-> DOM -CSS computing-> DOM with CSS -layout-> DOM with position -render-> Bitmap
-
-
-## Finite State Machine(FSM)
-
-- Moore: 每个机器都有确定的下一个状态
-- Mealy: 每个机器根据输入决定下一个状态
-
-
-## IOS-OSI 七层网络模型
-
-- 应用    - HTTP
-- 表示    - HTTP
-- 会话    - HTTP: require("http")
-- 传输    - TCP/IP: require("net")
-- 网络    - Internet
-- 数据链路 - 4G/5G/WIFI
-- 物理层   - 4G/5G/WIFI
-
-
-## HTTP Request
-
-POST / HTTP/1.1  (Request Line: Method Path Protocol)<br>
-Host: 127.0.0.1  (Headers: multiple lines end with an empty line.)<br>
-Content-Type: application/x-www-form-urlencoded<br>
-
-field1=aaa&code=x%3D1 (Body: write in the format of Content-Type)<br>
-
-
-## HTTP Response
-
-HTTP/1.1 200 OK          (Status Line: Protocol StatusCode StatusText)<br>
-Content-Type: text/html  (Headers: multiple lines end with an empty line.)<br>
-Date: Mon, 23 Dec 2019 06:46:19 GMT<br>
-Connection: keep-alive<br>
-Transfer-Encoding: chunked<br>
-
-26<br>                   (Body: chunked body start with a number and end with 0.)
-&#60html>&#60body>&#60/body>&#60/html><br>
-0<br>
-
-
-## Toy Browser
-
-- Request
-  根据上文 HTTP Request 结构设计API，并实现 Request 类。<br>
-  在 Request 构造器里收集必要信息，用 send 函数将收集到信息构建出的请求发送到服务器（server.js）。<br>
-  send 函数是异步的，所以使用 Promise 实现。<br>
-
-```javascript
 const net = require("net");
 
 class Request {
@@ -63,11 +9,11 @@ class Request {
     this.headers = options.headers || {};
     this.body = options.body || {};
 
-    if (!headers["Content-Type"]) {
-      headers["Content-Type"] = "application/x-www-form-urlencoded";
+    if (!this.headers["Content-Type"]) {
+      this.headers["Content-Type"] = "application/x-www-form-urlencoded";
     }
 
-    if (headers["Content-Type"] === "application/x-www-form-urlencoded") {
+    if (this.headers["Content-Type"] === "application/x-www-form-urlencoded") {
       this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join("&");
     } else if (headers["Content-Type"] === "application/json") {
       this.bodyText = JSON.stringify(this.body);
@@ -75,41 +21,6 @@ class Request {
 
     this.headers["Content-Length"] = this.bodyText.length;
   }
-
-  send() {
-    return new Promise((resolve, reject) => {});
-  }
-}
-
-void async function () {
-  let request = new Request({
-    method: "POST",     // HTTP
-    host: "127.0.0.1",  // IP
-    port: "8088",       // TCP
-    path: "/",          // HTTP
-    headers: {          // HTTP
-      ["X-Foo2"]: "customed"
-    },
-    body: {             // HTTP
-      name: "Shira",
-      date: "2021.01.17"
-    }
-  });
-
-  let response = await request.send();
-  console.log(response);
-}();
-```
-
-- Response
-  编写 send 函数，由于 Response 会逐步接收信息再构建，所以我们需要一个 ResponseParser。<br>
-  支持已有的 connection 或 自己新建 connection<br>
-  收到数据传给 parser<br>
-  根据 parser 返回的状态来判断完成时刻<br>
-
-```javascript
-class Request {
-  // ... constructor ...
 
   // construct request content
   toString() {
@@ -128,16 +39,22 @@ class Request {
         connection.write(this.toString());
       } else {
         // if has not connection, then create one to send request content.
+        console.log('create connection:', {
+          host: this.host,
+          port: this.port
+        })
         connection = net.createConnection({
           host: this.host,
           port: this.port
         }, () => {
+          console.log('Client Connected!');
+          console.log('write', this.toString())
           connection.write(this.toString());
         });
       }
 
       connection.on("data", data => {
-        console.log(data.toString())
+        console.log('connection on', data.toString())
         parser.receive(data.toString());
         if (parser.isFinished) {
           resolve(parser.response);
@@ -149,21 +66,14 @@ class Request {
         reject(err);
         connection.end();
       });
+
+      connection.on('end', () => {
+        console.log('Client end.');
+      });
     });
   }
 }
 
-class ResponseParser {
-  constructor() {}
-  receive(string) {}
-  receiveCharactor(charactor) {}
-}
-```
-
-- Response Parser
-  用状态机来分析文本结构，根据 HTTP Response 的结构来实现 parser 的状态机。<br>
-
-```javascript
 class ResponseParser {
   constructor() {
     this.WAITING_STATUS_LINE = 0;
@@ -186,7 +96,22 @@ class ResponseParser {
     this.bodyParser = null;
   }
 
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
+
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join("")
+    }
+  }
+
   receive(string) {
+    console.log('receive', string)
     for (let i = 0; i < string.length; i++) {
       this.receiveCharactor(string.charAt(i));
     }
@@ -212,6 +137,9 @@ class ResponseParser {
         this.current = this.WAITING_HEADER_SPACE;
       } else if (charactor === '\r') {
         this.current = this.WAITING_HEADER_BLOCK_END;
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new ThunkedBodyParser();
+        }
       } else {
         // collect a headers name
         this.headerName += charactor;
@@ -245,36 +173,9 @@ class ResponseParser {
         this.current = this.WAITING_BODY;
       }
     } else if (this.current === this.WAITING_BODY) {
-      console.log(charactor);
+      this.bodyParser.receiveCharactor(charactor);
     }
   }
-}
-```
-
-- Body Parser 
-  Response body 会因为 Content-Type 的不同有不同的结构，因此我们需要用子parser的结构来解决问题。<br>
-  以 ThunkedBodyParser 为例，用状态机实现 Body Parser。<br>
-
-```javascript
-class ResponseParser {
-  // ... constructor ...
-
-  get isFinished() {
-    return this.bodyParser && this.bodyParser.isFinished;
-  }
-
-  get response() {
-    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
-    return {
-      statusCode: RegExp.$1,
-      statusText: RegExp.$2,
-      headers: this.headers,
-      body: this.bodyParser.content.join("")
-    }
-  }
-
-  // ... receive ...
-  // ... receiveCharactor ...
 }
 
 class ThunkedBodyParser {
@@ -293,6 +194,7 @@ class ThunkedBodyParser {
 
   receiveCharactor(charactor) {
     if (this.current === this.WAITING_LENGTH) {
+      console.log('ThunkedBodyParser: ', 'WAITING_LENGTH')
       if (charactor === '\r') {
         if (this.length === 0) {
           this.isFinished = true;
@@ -328,4 +230,21 @@ class ThunkedBodyParser {
   }
 }
 
-```
+void async function () {
+  let request = new Request({
+    method: "POST",     // HTTP
+    host: "127.0.0.1",  // IP
+    port: "8088",       // TCP
+    path: "/",          // HTTP
+    headers: {          // HTTP, use key-value pair
+      ["X-Foo2"]: "customed"
+    },
+    body: {             // HTTP
+      name: "Shira",
+      date: "2021.01.17"
+    }
+  });
+
+  let response = await request.send();
+  console.log(response);
+}();
---
+++ Week08/toy-browser/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+http.createServer((request, response) => {
+  let body = [];
+  console.log('request:', request)
+  request.on('error', err => {
+    console.error(err);
+  }).on('data', chunk => {
+    console.log('http receive data:', chunk)
+    body.push(chunk.toString());
+  }).on('end', () => {
+    console.log('http receive end:')
+    body = Buffer.concat(body).toString();
+    console.log('body:', body);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(' Hello World\n');
+  });
+}).listen(8088);
+
+console.log('Server started');
