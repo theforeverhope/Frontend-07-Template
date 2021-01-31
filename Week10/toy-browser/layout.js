2b2dff8cd89b870dfc578adbe29d0950b56ec599
theforeverhope <theforeverhope@163.com>
2021-02-01 00:38:59 +0800

    feat: toy-browser with rendering DOM

--- Week10/toy-browser/layout.js
+++ Week10/toy-browser/layout.js
@@ -1,38 +1,3 @@
-学习笔记
-
-## 排版 - 预处理
-
-- flexDirection 和 wrap 相关属性
-  - flex-direction: row 
-    main: width x left right<br>
-    cross: height y top<br>
-  - flex-direction: column 
-    main: height y top<br>
-    cross: width x left right<br>
-
-```javascript
-// parser.js
-function emit(token) {
-  if (token.type === "startTag") {
-    // ...
-  } else if (token.type === "endTag") {
-    if (top.tagName !== token.tagName) {
-      throw new Error("Tag's start and end does not match!");
-    } else {
-      // encounter style tag, add css rules
-      // ...
-
-      // layout dom element
-      layout(top);
-      // ...
-    }
-    // ...
-  }
-}
-```
-
-```javascript
-// layout.js
 function getStyle(element) {
   if (!element.style) {
     element.style = {};
@@ -75,7 +40,7 @@ function layout(element) {
     }
   })
 
-  // set defualt value
+  // preprosess: set defualt value
   if (!elementStyle.flexDirection || elementStyle.flexDirection === 'layout')
     elementStyle.flexDirection = 'row';
 
@@ -144,24 +109,9 @@ function layout(element) {
     crossBase = 0;
     crossSign = 1;
   }
-}
-
-module.exports = layout;
-```
-
-
-## 排版 - 收集元素入行
-
-- 分行：根据主轴尺寸，把元素分入行。（若设置了'nowrap'，则强行分配进第一行。）
-- 如果父元素没有设置主轴尺寸，则由其子元素撑开width。（isAutoMainSize = true）
-
-```javascript
-// layout.js
-
-function layout(element) {
-  // ...preprocess...
 
   // flex line collect items
+
   let isAutoMainSize = false;
   if (!elementStyle[mainSize]) {
     elementStyle[mainSize] = 0;
@@ -182,8 +132,8 @@ function layout(element) {
     let item = items[i];
     let itemStyle = getStyle(item);
 
-    if (!item[mainSize]) {
-      item[mainSize] = 0;
+    if (!itemStyle[mainSize]) {
+      itemStyle[mainSize] = 0;
     }
 
 
@@ -221,26 +171,9 @@ function layout(element) {
       mainSpace -= itemStyle[mainSize];
     }
   }
-}
-```
-
-
-## 布局 - 计算主轴
-
-- 找出所有flex元素
-- 把主轴剩余尺寸等比分给剩余元素
-- 若剩余空间为负，所有flex元素为0，等比压缩剩余元素
-
-```javascript
-// layout.js
-
-function layout(element) {
-  // ...preprocess...
-
-  // ... flex line collect items ...
 
   // compute main axis sise - mainSpace
-    flexLine.mainSpace = mainSpace;
+  flexLine.mainSpace = mainSpace;
 
   if (elementStyle.flexWrap === 'nowrap' || isAutoMainSize) {
     flexLine.crossSpace = elementStyle[crossSize] !== undefined ? elementStyle[crossSize] : crossSpace;
@@ -323,25 +256,6 @@ function layout(element) {
       }
     });
   }
-}
-```
-
-
-## 布局 - 计算交叉轴
-
-- 根据每一行中最大元素尺寸计算行高。
-- 根据行高flex-align和item-align，确定元素位置。
-- 修改server，返回flex布局的待解析页面代码。
-
-```javascript
-// layout.js
-
-function layout(element) {
-  // ...preprocess...
-
-  // ... flex line collect items ...
-
-  // ... compute main axis sise - mainSpace ...
 
   // compute cross axis size - crossSpace
   crossSpace = 0;
@@ -412,6 +326,7 @@ function layout(element) {
         itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
       } else if (align === 'stretch') {
         itemStyle[crossStart] = crossBase; 
+        // itemStyle[crossEnd] = crossBase + crossSign * ((itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)));
         itemStyle[crossEnd] = crossBase + crossSign * lineCrossSize;
         itemStyle[crossSize] = crossSign * (itemStyle[crossEnd] - itemStyle[crossStart]);
       }
@@ -419,155 +334,7 @@ function layout(element) {
 
     crossBase += crossSign * (lineCrossSize + step);
   })
-}
-```
-
-```javascript
-// server.js
-http.createServer((request, response) => {
-  let body = [];
-  request.on('error', err => {
-    // ...
-  }).on('data', chunk => {
-   // ...
-  }).on('end', () => {
-    // ...
-    response.end(`<html maaa=a >
-    <head>
-      <style>
-      #container {
-        width:500px;
-        height:300px;
-        display:flex;
-      }
-      #container #myid {
-        width:200px;
-      }
-      #container .c1 {
-        flex: 1;
-      }
-      </style>
-    </head>
-    <body>
-      <div id="container">
-        <div id="myid" />
-        <div class="c1" />
-      </div>
-    </body>
-    </html>`);
-  });
-}).listen(8088, () => {
-  // ...
-});
-```
-
-
-## 渲染 - 绘制单个元素
-
-- 准备图形环境：用生成图片代替绘制到屏幕（使用库images)
-- server 给 div 上色，便于查看绘制效果
-
-```javascript
-// server.js
-http.createServer((request, response) => {
-  let body = [];
-  request.on('error', err => {
-    // ...
-  }).on('data', chunk => {
-   // ...
-  }).on('end', () => {
-    // ...
-    response.end(`<html maaa=a >
-    <head>
-      <style>
-      #container {
-        width:500px;
-        height:300px;
-        display:flex;
-        background-color: rgb(255,255,255);
-      }
-      #container #myid {
-        width:200px;
-        height:100px;
-        background-color: rgb(255,0,0);
-      }
-      #container .c1 {
-        flex: 1;
-        background-color: rgb(0,255,0);
-      }
-      </style>
-    </head>
-    <body>
-      <div id="container">
-        <div id="myid" />
-        <div class="c1" />
-      </div>
-    </body>
-    </html>`);
-  });
-}).listen(8088, () => {
-  // ...
-});
-```
-
-```javascript
-// client.js
-const images = require("images");
-
-// ... class: Request ResponseParser ThunkedBodyParser 
-
-function render(viewport, element) {
-  if (element.style) {
-    var img = images(element.style.width, element.style.height);
-
-    if (element.style["background-color"]) {
-      let color = element.style["background-color"] || "rgb(0,0,0)";
-      color.match(/rgb\((\d+),(\d+),(\d+)\)/);
-      img.fill(Number(RegExp.$1), Number(RegExp.$2), Number(RegExp.$3));
-      viewport.draw(img, element.style.left || 0, element.style.top || 0);
-    }
-  }
+  
 }
 
-void async function () {
-  // ... 
-
-  let viewport = images(800, 600);
-  render(viewport, dom.children[0].children[3].children[1].children[3])
-  viewport.save("viewport.jpg")
-}();
-
-```
-
-
-## 渲染 - 绘制Dom树
-
-- 递归调用子元素的绘制方法完成Dom树绘制
-- 忽略不需要绘制的节点
-- 实际浏览器中，文字绘制是难点，需要依赖字体库（把字体变成图片去渲染），这里先忽略。
-- 实际浏览器中，还会对一些图层做compositing，这里也忽略了。
-
-```javascript
-// client.js
-const images = require("images");
-
-// ... class: Request ResponseParser ThunkedBodyParser 
-
-function render(viewport, element) {
-  // ... if (element.style) { ... }
-
-  if (element.children) {
-    for(let child of element.children) {
-      render(viewport, child);
-    }
-  }
-}
-
-void async function () {
-  // ... 
-
-  let viewport = images(800, 600);
-  render(viewport, dom)
-  viewport.save("viewport_all.jpg")
-}();
-```
+module.exports = layout;
